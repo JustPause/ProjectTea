001ef02891caafb069ac234f8d7c83c0c4e8b9e0
JustPause <iamjuststan@hotmail.com>
2024-04-22 01:02:00 +0300

    init push

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import MainPage from './components/MainPage.vue'
+</script>
+
+<template>
+  <header>
+    
+    <NavBar />
+  </header>
+
+  <main>
+    <MainPage />
+  </main>
+
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div> 
+</template>
+
+<style scoped>
+
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+
+  position:fixed;
+  top:0;
+  color: white;
+  width:100%;
+  left:0;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+</style>
