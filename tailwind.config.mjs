0e7721fe4907b0226313f9e6eaedd693932cebb4
JustPause <Justinaslolt@gmail.com>
2024-09-16 00:21:22 +0300

    ba

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        textColor: {
+          light: "#005600",
+          dark: "#A7BF8F",
+        },
+        mainColor: {
+          light: "#005600",
+          dark: "#A7BF8F",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
