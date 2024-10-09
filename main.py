dc2ad557d48a42b01ab5dc2fd74b1a36a0718db8
JustPause <iamjuststan@hotmail.com>
2024-10-10 01:18:57 +0300

    okat

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, render_template_string, send_from_directory
 
-app = Flask(__name__,
-    template_folder='Front-End/dist',
-    static_folder='Front-End/dist/assets')
+app = Flask(__name__, static_folder='Front-End/dist', static_url_path='')
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return send_from_directory('Front-End/dist', 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
