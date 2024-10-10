37be2845103f8313df821624f3630608a6924a07
JustPause <iamjuststan@hotmail.com>
2024-10-10 03:18:49 +0300

    pyah

--- Front-End/tailwind.config.mjs
+++ Front-End/tailwind.config.mjs
@@ -2,17 +2,25 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        textColor: {
-          light: "#394132",
-          dark: "#A7BF8F",
-        },
-        mainColor: {
-          light: "#A7BF8F",
-          dark: "#394132",
-        },
-      },
+    colors: {
+      'white': '#ffffff',
+      'light': '#f4f0e5',
+      'color': '#b8cfa3',
+      'dark-green': '#394132',
+      'black': '#303030',
+      'accent': '#a62f03',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    spacing: {
+      'Headline-1': '5.852rem',
+      'Headline-2': '4.18rem',
+      'Headline-3': '2.5rem',
+      'Subheadline': '1.67rem',
+      'Text': '1.33rem',
+      'Caption': '1rem'
     },
   },
   plugins: [],
