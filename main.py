e90df1fa4a891020e074add6711eb0ce97595841
JustPause <iamjuststan@hotmail.com>
2024-10-11 05:51:19 +0300

    Boom

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='Front-End/dist', static_url_path='')
 def index():
     return send_from_directory('Front-End/dist', 'index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+  return send_from_directory('Front-End/dist', '404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
