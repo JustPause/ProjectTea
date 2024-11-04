11f23ec89a33eb6b916e968d7ad8cf8b6ac2b4d0
JustPause <iamjuststan@hotmail.com>
2024-11-05 00:40:53 +0200

    Brum

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from flask import Flask, render_template, render_template_string, send_from_directory
-
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='Front-End/dist', static_url_path='')
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fruits.db'
-
-db = SQLAlchemy(app)
-db.init_app(app)
 
 @app.route("/")
 def index():
@@ -17,8 +9,6 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
   return send_from_directory('Front-End/dist', '404.html'), 404
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
