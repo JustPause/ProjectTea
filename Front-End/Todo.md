a5a7442d32e0b84d1ff6536cdb857a04318e576f
JustPause <iamjuststan@hotmail.com>
2024-11-05 02:02:35 +0200

    Big uPdate, and nwo sleep, bye

---
+++ Front-End/Todo.md
@@ -0,0 +1,13 @@
+# TODO
+
+- [x] Headerio miktuvai visi kaip ir veikia
+- [ ] Iskojijmo funcija gera, bet neuveda i po paskaudimo i taji puslapi su datos duomenimis
+- [ ] Andtros dalies arbatu pasirinkimas neatitinka standartu
+  - [ ] Jis neturi iskarto parinktos arbatos, kuri turtu buti Green tea
+  - [ ] Duomenis po paspaudimo nepasikeicia
+  - [ ] ir arbatos nuotraukos nei nuveda i arbatos puslapi ne leidziasi buti paspaustos
+- [ ] Discover nenuveda link nekur nenuveda
+- [ ] Receptai po paspaudimo neissilecia, ir nesiskiria vienas nuo kito
+- [ ] Receptu miktukas nuveda neikur
+- [ ] Asmenimis puslapis neegzstuoja
+- [ ] Meet the tea-m nuveda neras imanomas paspausti
--- main.py
+++ main.py
@@ -5,6 +5,30 @@ app = Flask(__name__, static_folder='Front-End/dist', static_url_path='')
 @app.route("/")
 def index():
     return send_from_directory('Front-End/dist', 'index.html')
+  
+@app.route("/benefits")
+def benefits():
+    return send_from_directory('Front-End/dist', 'benefits/index.html')
+  
+@app.route("/profile")
+def profile():
+    return send_from_directory('Front-End/dist', 'profile.html')
+  
+@app.route("/recipes")
+def recipes():
+    return send_from_directory('Front-End/dist', 'recipes.html')
+  
+@app.route("/singleTea")
+def singleTea():
+    return send_from_directory('Front-End/dist', 'singleTea.html')
+  
+@app.route("/teaList")
+def teaList():
+    return send_from_directory('Front-End/dist', 'teaList.html')
+  
+@app.route("/team")
+def team():
+    return send_from_directory('Front-End/dist', 'team.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
